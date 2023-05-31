64177e1575e08ab25040496b8ed9e6bb77c7fff0
letsgosmokes <saroy032@gmail.com>
2023-05-31 14:22:19 +0530

    adding footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./Header"
 import Meme from "./Meme";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header name="Meme App" />
         <Meme />
+        <Footer name="my foot" />
       </div>
     );
   }
